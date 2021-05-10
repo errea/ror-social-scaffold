2453dcb90c57f0d6545bccc73ba5406a03daa21b
Mihndim <jelsieprisca2016@gmail.com>
2021-05-10 15:43:59 -0400

    Add user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def create
+  end 
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
