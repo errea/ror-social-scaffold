9696e3240e34f373e70dc995cea298369d65881b
Mihndim <jelsieprisca2016@gmail.com>
2021-05-11 17:33:01 -0400

    Add Friendship helper methods for pending friends and friend request

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,11 @@
 module FriendshipsHelper
+  # Users who are yet to be confirmed as friends
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
+  end
 end
