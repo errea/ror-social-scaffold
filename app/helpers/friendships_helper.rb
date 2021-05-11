43bcbd56b057cdba7f346f6cba37ae69b70a108a
Mihndim <jelsieprisca2016@gmail.com>
2021-05-11 18:01:36 -0400

    Add helper methods to confirm a friend request and a method to check if a given user is a friend

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -8,4 +8,16 @@ module FriendshipsHelper
   def friend_requests
     inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
   end
+
+  # Method to confirm friend request
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = true
+    friendship.save
+  end
+
+  # Method to check if a given user is a friend
+  def friend?(user)
+    friends.include?(user)
+  end
 end
