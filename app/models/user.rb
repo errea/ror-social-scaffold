0e9535d0ae9e39ab63ab52a347d0ac543d45966c
errea <erreakezy@gmail.com>
2021-05-13 00:54:27 +0100

    Modify user model to reflect pending,inverse & confirmed request

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { minimum: 6 }, uniqueness: true
 
   has_many :posts
   has_many :comments, dependent: :destroy
@@ -12,30 +13,5 @@ class User < ApplicationRecord
   has_many :friendships
 
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    friends_array.compact
-  end
-
-  # Users who have yet to confirme friend requests
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  # Users who have requested to be friends
-  def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  end
-
-  def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
-    friendship.confirmed = true
-    friendship.save
-  end
-
-  def friend?(user)
-    friends.include?(user)
-  end
+  has_many :friend_requests, through: :inverse_friendships, source: :user
 end
