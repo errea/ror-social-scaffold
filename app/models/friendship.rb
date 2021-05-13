b58a5293cc36737d36f88b68d36969be76d842c3
errea <erreakezy@gmail.com>
2021-05-13 13:53:08 +0100

    Modify friendship models + add private methods to confirm and create friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,26 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  validate :users_friends
+
+  after_update :create_reverse_friendship
+
+  private
+
+  def users_friends
+    # rubocop:disable Layout/LineLength
+    errors.add(:base, 'Already friends!') if (Friendship.where(user_id: friend_id, friend_id: user_id, status: true).exists? && Friendship.where(user_id: user_id, friend_id: friend_id, status: true).exists?) || user_id == friend_id
+  end
+
+  def create_reverse_friendship
+    reverse_fs = Friendship.new(user_id: friend_id, friend_id: user_id, status: true)
+    reverse_fs.save
+    # rubocop:enable Layout/LineLength
+  end
+
+  def confirm_friend
+    update_attributes(confirmed: true)
+    Friendship.create!(friend_id: user_id, user_id: friend_id, confirmed: true)
+  end
 end
