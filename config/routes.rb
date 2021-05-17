a952be6f529c8da38ab455e7b7ebbcb5fffb981c
errea <erreakezy@gmail.com>
2021-05-17 20:29:31 +0100

    Add test for friendships with 2 rows for mutual friendship

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-
+  post 'invite', to: 'friendships#create'
+  put 'invite', to: 'users#update'
+  delete 'reject', to: 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
