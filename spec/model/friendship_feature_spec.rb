e71f53b8efc9649237dffc609f42c80deb57353c
errea <erreakezy@gmail.com>
2021-05-25 21:10:11 +0100

    Modify readme

--- spec/model/friendship_feature_spec.rb
+++ spec/model/friendship_feature_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-#friendship feature
+# friendship feature
 RSpec.feature 'Friendship invitation', type: :feature do
   before(:each) do
     @user1 = User.create(name: 'eri', email: 'eri@.com', password: '123456')
